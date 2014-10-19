9ca5855159b5a1d5e4c8056187071ea7b29442b7
Fabio Marini <fabio.mariniss@gmail.com>
2014-10-19 16:25:15 +0200

    Initial import for Heroku

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -52,6 +52,9 @@ grails.project.dependency.resolution = {
 		runtime 'mysql:mysql-connector-java:5.1.29'
 		
 		test "org.grails:grails-datastore-test-support:1.0-grails-2.4"
+
+        // Heroku dependencies
+        runtime 'org.postgresql:postgresql:9.3-1101-jdbc41' 
     }
 
     plugins {
@@ -76,6 +79,9 @@ grails.project.dependency.resolution = {
 		
 		compile ":spring-security-core:2.0-RC4"
 		
-		runtime 'postgresql:postgresql:8.4-702.jdbc3'
+        // Heroku dependencies
+        compile ':heroku:1.0.1'
+        compile ':cloud-support:1.0.8'
+        compile ':webxml:1.4.1'
     }
 }
